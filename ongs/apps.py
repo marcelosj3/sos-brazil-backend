1c5718657a42f509a22e4a16558686a80d2b7ab7
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-07-20 14:22:11 -0300

    fix: file name to ongs

--- ongs/apps.py
+++ ongs/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OngConfig(AppConfig):
+class OngsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "ong"
+    name = "ongs"
