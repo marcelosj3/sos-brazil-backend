6cf134f43094da847f4306539d51f3a7a66eeccb
Gabriel <gabriel.such@hotmail.com>
2022-07-20 14:35:21 -0300

    fix: fixed id to none

--- causes/models.py
+++ causes/models.py
@@ -5,3 +5,5 @@ from uuid import uuid4
 class CauseModel(models.Model):
     cause_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=100, null=False, unique=True)
+
+    id = None
