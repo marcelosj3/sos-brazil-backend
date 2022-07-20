db752ed99879a42df810dd560b77892f2a745f74
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-07-20 14:07:44 -0300

    feature: add ong_uuid in Ong models

--- ong/apps.py
+++ ong/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class OngConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ong'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ong"
--- ong/models.py
+++ ong/models.py
@@ -1,11 +1,14 @@
+from uuid import uuid4
+
 from django.db import models
 
 
 # Create your models here.
 class Ongs(models.Model):
-    name= models.CharField(max_length= 100, null= True, unique= True)
-    description= models.CharField(max_length= 255, null= True, unique= True)
-    cnpj= models.CharField(max_length= 14, null= True, unique= True)
-    site_address= models.CharField(max_length= 255)
-    logo= models.CharField(max_length= 255, null= True)
-    created_at= models.DateField(auto_now= True) 
+    ong_uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100, null=True, unique=True)
+    description = models.CharField(max_length=255, null=True, unique=True)
+    cnpj = models.CharField(max_length=14, null=True, unique=True)
+    site_address = models.CharField(max_length=255)
+    logo = models.CharField(max_length=255, null=True)
+    created_at = models.DateField(auto_now=True)
