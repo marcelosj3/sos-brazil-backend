e0301a0682f129892a61286545f1807cc02d3010
Marcelo Silveira <marcelosj3@hotmail.com>
2022-07-20 09:55:29 -0300

    fix: run black to format files

--- sos_brazil/asgi.py
+++ sos_brazil/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sos_brazil.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sos_brazil.settings")
 
 application = get_asgi_application()
