29008f62f7c54c695943c9dbc24c2e99be24a730
Marcelo Silveira <marcelosj3@hotmail.com>
2022-07-20 13:51:32 -0300

    feat: custom user manager, settings and add model columns
    Created custom user manager;
    Added default auth user model and users apps into project settings;
    Added created_at and updated_at columns to the model.

--- users/models.py
+++ users/models.py
@@ -12,9 +12,12 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     id = None
     username = None
+    date_joined = None
 
     USERNAME_FIELD = "email"
 
