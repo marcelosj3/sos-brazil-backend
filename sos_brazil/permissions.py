30057fa66307f89f2e856cc7892b7c7a84a2352e
Marcelo Silveira <marcelosj3@hotmail.com>
2022-07-22 12:31:59 -0300

    feat: change logic and specify the permission
    Changed the logic to fit the user list create view;
    Specified the permission into the users folder.

--- sos_brazil/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-from rest_framework.views import Request
-
-
-class IsSuperuser(BasePermission):
-    def has_permission(self, request: Request, _):
-        if request.user.is_superuser:
-            return True
