e3efc9484229d5561b9f8598556a42adc23f6ae7
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-07-22 08:13:12 -0300

    feature: add secret_key in .env

--- sos_brazil/settings.py
+++ sos_brazil/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,6 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = getenv("SECRET_KEY")
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
