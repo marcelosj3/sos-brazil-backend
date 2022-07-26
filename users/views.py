7d2067d185b49d71703cb121964e814878d4d856
Marcelo Silveira <marcelosj3@hotmail.com>
2022-07-26 11:14:57 -0300

    feat: get user by id view and permission

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from users.models import User
+from users.permissions import IsSuperuserListCreateUser, IsSuperuserOrUser
 
 from .serializers import UserLoginSerializer, UserSerializer
 
@@ -11,5 +14,18 @@ class UserLoginView(ObtainAuthToken):
 
 
 class UserView(ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsSuperuserListCreateUser]
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserIdView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsSuperuserOrUser]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    lookup_field = "user_id"
