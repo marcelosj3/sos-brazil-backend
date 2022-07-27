e47d2d9376b758cd36625f144bf41857a5a11285
Marcelo Silveira <marcelosj3@hotmail.com>
2022-07-27 15:31:52 -0300

    fix: ong permissions and causes exceptions

--- campaigns/views.py
+++ campaigns/views.py
@@ -1,12 +1,12 @@
 from django.core.exceptions import ValidationError
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-from ongs.models import Ong
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView, Request, Response, status
 
 from campaigns.permissions import CampaignPermission
 from campaigns.serializers import CampaignSerializer, DonationSerializer
+from ongs.models import Ong
 
 from .models import Campaign
 
