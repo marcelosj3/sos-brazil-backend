267f210e9134a34292721b730ea02d709d43090a
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-07-28 16:46:42 -0300

    fix: Causes bug creating a keyTypeException

--- sos_brazil/exceptions.py
+++ sos_brazil/exceptions.py
@@ -38,3 +38,19 @@ class InvalidKeyException(APIException):
 
         self.detail = f"{key.title()} cannot be updated."
         self.status_code = status_code
+
+
+class KeyTypeError(APIException):
+    default_detail = "Expected a list of items"
+
+    def __init__(
+        self,
+        key: str,
+        message: str,
+        status_code: int = status.HTTP_400_BAD_REQUEST,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.detail = {key.lower(): message}
+        self.status_code = status_code
